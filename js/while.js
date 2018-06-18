aeb8ec7f50b980bb3644200833e13bf72e25bc61
Linda M Herrera <lindamherrera@Lindas-MacBook-Air.local>
2018-06-18 13:38:29 -0500

    completed while loop exercise

---
+++ js/while.js
@@ -0,0 +1,34 @@
+"use strict";
+
+
+
+// Create a while loop that uses console.log() to create the output shown below.
+//
+//
+// 2
+// 4
+// 8
+// 16
+// 32
+// 64
+// 128
+// 256
+// 512
+// 1024
+// 2048
+// 4096
+// 8192
+// 16384
+// 32768
+// 65536
+
+
+
+
+var x = 2;
+while (x <=65536){
+    console.log(x);
+    x = x * 2;
+}
+
+
