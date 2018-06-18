ad7c5cf5a44943805c519721a0aa76a3745d89be
Linda M Herrera <lindamherrera@Lindas-MacBook-Air.local>
2018-06-18 12:14:32 -0500

    cleaning branch tree

---
+++ js/js_pract_test.js
@@ -0,0 +1,34 @@
+"use strict";
+
+// // console.log("it works!");
+//
+// function calculator(num1, str, num2, round) {
+//     if (str === 'add') {
+//         return num1 + num2;
+//     }
+//     else if (str === 'sub') {
+//         return num1 - num2;
+//     }
+//     var sum = parseInt(num1) + "add" + parseInt(num2);
+//     // Math.round(round); // <- find out where to pass the number of digits as a param here
+//     // return round
+// }
+//
+// // console.log("round");
+//
+// console.log(calculator(2, 'add', 2, 2));
+
+// *******************************
+// function isBoolean(input) {
+//     return typeof input == "boolean";
+// }
+//
+//
+//
+// function isTrue (input) {
+//     var userInput = (input == "boolean");
+//     return userInput;
+// }
+
+//
+// *******************************
