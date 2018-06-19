c383bb1d2f2994425de745d4dab6a19a544edbfb
Linda M Herrera <lindamherrera@Lindas-Air.attlocal.net>
2018-06-18 22:24:51 -0500

    random number generate exercise

--- js/for_loops.js
+++ js/for_loops.js
@@ -3,28 +3,24 @@
 // console.log("just checking");
 
 
-// Create a function named showMultiplicationTable that accepts a number and console.logs the multiplication table for that number (just multiply by the numbers 1 through 10)
-//
-// For example, showMultiplicationTable(7) should output
-//
-//
-// 7 x 1 = 7
-// 7 x 2 = 14
-// 7 x 3 = 21
-// 7 x 4 = 28
-// 7 x 5 = 35
-// 7 x 6 = 42
-// 7 x 7 = 49
-// 7 x 8 = 56
-// 7 x 9 = 63
-// 7 x 10 = 70
+
 
 function showMultiplicationTable(number){
-    var a = 7;
 
-    for (var i = 1; i<=10 ; i++) {
-       var b = i * a;
-        console.log(a+ "*" +i+ "="+b) ;
+    for (var i = 1; i <= 10 ; i++) {
+       var b = number * i;
+        console.log(number + " * " + i + " = " + b) ;
     }
 
+}
+
+
+
+for (var i = 0; i < 10; i++ ) {
+    var randomNum = (Math.floor(Math.random() * 200) + 20);
+    if (randomNum % 2 === 0 ){
+        console.log (randomNum + " even");
+    } else {
+        console.log (randomNum + " odd");
+    }
 }
