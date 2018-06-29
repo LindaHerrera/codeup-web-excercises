95adfc077ea42e30472096410fa8a1aa4f61685c
Linda M Herrera <lindamherrera@Lindas-MacBook-Air.local>
2018-06-29 14:44:44 -0500

    checking tree

--- jquery_exercises.js
+++ jquery_exercises.js
@@ -1,20 +1,36 @@
 "use strict";
 
-console.log("test");
+// console.log("test");
 
 
 $(function(){
     $("#playButton").click(function(){
         $("#playOutput").text($("#playInput").val());
+    });
+// $(".codeup").on("mouseover", function (){
+//     $(this).css("background-color", "yellow");
+// })
+
+        // $("li").css("font-size", "20px").css("background-color", "aqua");
+        // $("p").css("background-color", "salmon");
+        // $("h1").css("background-color", "papayawhip");
+
+    $("h1").click(function () {
+        $(this).css("background-color", "papayawhip");
+    });
+
+    $("p").dblclick(function(){
+        $(this).css("font-size", "18px");
+    });
+
+
+
+    $("li").hover(function() {
+        $(this).css("color", "red");
+    }, function (){
+        $(this).css("color", "black");
     })
-$("p").on("mouseover", function (){
-    $(this).css("background-color", "yellow");
-})
-
-        alert($("#corgi").text());
-
-
-})
+});
 
 
 
