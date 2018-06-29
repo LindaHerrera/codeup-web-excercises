c46b2771d912b5f95a8a2f9d1d9cfb00dc8588d3
Linda M Herrera <lindamherrera@Lindas-MacBook-Air.local>
2018-06-29 10:31:13 -0500

    jquery exercise

---
+++ jquery_exercises.js
@@ -0,0 +1,20 @@
+"use strict";
+
+console.log("test");
+
+
+$(function(){
+    $("#playButton").click(function(){
+        $("#playOutput").text($("#playInput").val());
+    })
+$("p").on("mouseover", function (){
+    $(this).css("background-color", "yellow");
+})
+
+        alert($("#corgi").text());
+
+
+})
+
+
+
