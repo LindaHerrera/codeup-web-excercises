ce6e4ceb1e142863c0f51171e58afacfef9a15f5
Linda M Herrera <lindamherrera@Lindas-MacBook-Air.local>
2018-07-01 10:55:21 -0500

    commented code and correct if statement without loop

--- js/konami-js.js
+++ js/konami-js.js
@@ -1,5 +1,50 @@
 "use strict";
 
-$(document).keyup(function(event){
-    console.log(event.keyCode);
-});
+/*
+ $(document).keyup(function(event){
+     console.log(event.keyCode);
+ });
+
+
+ target area; body of the page, when keys are pressed, compare EACH event to this string:
+         xx xx xx xx xx  x x x. if string matches this
+             then do this
+
+ https://stackoverflow.com/questions/3278556/for-loop-with-if-else-statement
+*/
+
+// $(document).keyup(function(event){
+//     console.log(event.keyCode);
+//
+// });
+//
+//
+// // loop to compare to my array
+// // if true do this
+//
+// // up == 38
+
+$(document).ready(function(){
+
+    // Array sequence for correct Konami code
+    var code = [38, 38, 40, 40, 37, 39, 37,39, 66, 65, 13];
+
+    var counter = 0;
+
+    $(document).keyup(function(event){
+        console.log(event.keyCode);
+
+        if (event.keyCode == code[counter]) {
+            counter++;
+            console.log(counter);
+        } else {
+            counter = 0;
+            console.log(counter);
+
+        }
+        if (counter == code.length) {
+            console.log("pass");
+        }
+    });
+})
+
