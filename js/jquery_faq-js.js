5a28146260676d0e692b4761b78bd7b8a05e3475
Linda M Herrera <lindamherrera@Lindas-MacBook-Air.local>
2018-07-03 10:47:05 -0500

    last exercise item done

--- js/jquery_faq-js.js
+++ js/jquery_faq-js.js
@@ -1,18 +1,33 @@
+"use strict";
+
 $(function(){
 
 
-$(".show").click(function (){
-    // event.preventDefault();
+    $(".show").click(function (){
+        // event.preventDefault();???
         $("dd").toggleClass("invisible");
+    });
 
-});
+    $(".sty").click(function(){
 
-$(".sty").click(function(){
+        $("ul li:last-child").css("background-color", "yellow");
+    });
 
-    $("ul li:last-child").css("background-color", "yellow");
-})
+    $(".park").click(function(){
+        $(this).next().css("font-weight", "bold");
+    });
 
+    $(".park2").click(function(){
+        $(this).next().css("font-weight", "bold");
+    });
 
+    $(".park3").click(function(){
+        $(this).next().css("font-weight", "bold");
+    });
+
+    $("li").click(function(){
+        $(this).parent().children().first().css("color", "blue");
+    })
 
 
 });
