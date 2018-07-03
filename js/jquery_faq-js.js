37ae645fde737c93b260509a7bf5bdd4e5bc4bf2
Linda M Herrera <lindamherrera@Lindas-MacBook-Air.local>
2018-07-03 12:28:44 -0500

    add notes in JavaScript to organize code

--- js/jquery_faq-js.js
+++ js/jquery_faq-js.js
@@ -2,28 +2,28 @@
 
 $(function(){
 
-
+//shows dd's with class of invisible when button is clicked
     $(".show").click(function (){
         // event.preventDefault();???
         $("dd").toggleClass("invisible");
     });
-
+//highlightslast li of each ul when button clicked
     $(".sty").click(function(){
         $("ul li:last-child").css("background-color", "yellow");
     });
-
+//bolds all li items when respective h3 is clicked
     $(".park").click(function(){
         $(this).next().css("font-weight", "bold");
     });
-
+//bolds all li items when respective h3 is clicked
     $(".park2").click(function(){
         $(this).next().css("font-weight", "bold");
     });
-
+/// bolds all li items when respective h3 is clicked
     $(".park3").click(function(){
         $(this).next().css("font-weight", "bold");
     });
-
+//when any li is clicked the first li of that list changes color
     $("li").click(function(){
         $(this).parent().children().first().css("color", "blue");
     })
