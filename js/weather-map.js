bebe224b0207ebab60409c36514a58f81c96a501
Linda M Herrera <lindamherrera@Lindas-MacBook-Air.local>
2018-07-11 11:38:58 -0500

    bold temp and add degree symbol

--- js/weather-map.js
+++ js/weather-map.js
@@ -8,7 +8,11 @@ $(document).ready(function(){
         units: "imperial"
     });
 
+
     var tableBody = $("#rowOne");
+    // var pict = "<img src = "http://openweathermap.org/img/w/>";
+
+
 
     jgXhr.done(function(data){
         console.log(data);
@@ -16,11 +20,11 @@ $(document).ready(function(){
             // var hum = data.list[i].main.humidity;        //humidity for 3 days in variable
             // console.log(hum);
             var html = "<div class='card-body card d-inline float-left text-center mx-auto w-25 p-2 my-3'>";
-            html += "<div>" +"Temp "+ data.list[i].main.temp + "</div>";
+            html += "<div>" +"<strong>"+"<h2>"+Math.round(data.list[i].main.temp)+ "&deg"+"</h2>"+"</strong>" + "</div>";
             html += "<div>" + "<strong>Clouds: </strong>"+ data.list[i].weather[0].description + "</div>";     //clouds in array so place array name in array brackets
             html += "<div>" + "<strong>Humidity: </strong>"+ data.list[i].main.humidity +"%"+ "</div>";
-            html += "<div>" + "<strong>Wind: </strong>"+ data.list[i].wind.speed + "</div>";
-            html += "<div>" + "<strong>Pressure: </strong>" + data.list[i].main.pressure +"</div>";
+            html += "<div>" + "<strong>Wind: </strong>"+ Math.round(data.list[i].wind.speed) +"mph"+"</div>";
+            html += "<div>" + "<strong>Pressure: </strong>" +data.list[i].main.pressure +"</div>";
             html += "</div>";
             tableBody.append(html);
         }
